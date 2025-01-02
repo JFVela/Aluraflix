0f78feb3b200d14e876f5ab45a37a61f0d91c2e9
JFVela <jfvela_2012@hotmail.com>
2025-01-02 18:02:42 -0500

    Initial commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import Logo from "./Logo.png";
+import styles from "./Header.module.css";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+
+const Header = () => {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (button) => {
+    setSelectedButton((prevButton) => (prevButton === button ? null : button));
+  };
+
+  const renderButton = (label, buttonKey) => (
+    <Button
+      variant="outlined"
+      className={`${styles.button} ${
+        selectedButton === buttonKey ? styles.active : ""
+      }`}
+      onClick={() => handleButtonClick(buttonKey)}
+      style={{
+        color: "white",
+        borderColor: "white",
+      }}
+    >
+      {label}
+    </Button>
+  );
+
+  return (
+    <header className={styles.header}>
+      <section className={styles.logoContenido}>
+        <img src={Logo} alt="Aluraflix" />
+      </section>
+      <div>
+        <Stack direction="row" spacing={3}>
+          {renderButton("Home", "home")}
+          {renderButton("Nuevo Video", "nuevo")}
+        </Stack>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
